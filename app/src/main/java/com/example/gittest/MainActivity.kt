ef5fe162cff86832622babc4b2c31045365081bf
Mahdi S.F <meddisommer@gmail.com>
2023-02-13 21:39:14 +0330

    toast in activity main has been change

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.gittest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "hey", Toast.LENGTH_SHORT).show()
     }
+    
 }
